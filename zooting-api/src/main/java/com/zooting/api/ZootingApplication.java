1e2846bb868db627199ab4fe44fb9a1eadc6b7cd
duckbill413 <uhyeon7399@daum.net>
2024-01-15 11:09:02 +0900

    [BE] Spring initial commit

---
+++ zooting-api/src/main/java/com/zooting/api/ZootingApplication.java
@@ -0,0 +1,13 @@
+package com.zooting.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZootingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZootingApplication.class, args);
+	}
+
+}
---
+++ zooting-api/src/test/java/com/zooting/api/ZootingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zooting.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZootingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
