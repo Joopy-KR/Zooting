de1a2d7ac7defc150eddbf3a35f2b573837fe6e6
2yunj007 <2yunj007@gmail.com>
2024-01-16 17:56:52 +0900

    feat: 성격 테스트 페이지 - 진행 중

--- zooting/src/App.vue
+++ zooting/src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
+    <RouterView></RouterView>
   </div>
 </template>
 
 <script setup lang="ts">
-
+import { RouterView } from 'vue-router'
 </script>
 
 <style scoped>
--- zooting/src/views/PersonalityTestView.vue
+++ zooting/src/views/PersonalityTestView.vue
@@ -1,10 +1,43 @@
 <template>
     <div>
         <h1>Personality Test</h1>
+        <!-- Description -->
+        <div>
+            <Description></Description>
+        </div>
+
+        <!-- MBTI input -->
+        <div>
+            <MBTIInput></MBTIInput>
+        </div>
+
+        <!-- 남은 문제 수 -->
+        <div>
+        </div>
+
+        <!-- Qusetion -->
+        <div>
+            <Question></Question>
+        </div>
+
+        <!-- Answer select button -->
+        <div>
+            <AnswerButton></AnswerButton>
+        </div>
+
+        <!-- Result -->
+        <div>
+            <Result></Result>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import Description from '@/components/personality-test/Description.vue'
+import MBTIInput from '@/components/personality-test/MBTIInput.vue'
+import Question from '@/components/personality-test/Question.vue'
+import AnswerButton from '@/components/personality-test/AnswerButton.vue'
+import Result from '@/components/personality-test/Result.vue'
 
 </script>
 
