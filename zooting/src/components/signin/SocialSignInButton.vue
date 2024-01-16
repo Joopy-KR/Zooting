13716e7f1c5e1ecc4d6b65f92bf351f904cc21af
duckbill413 <uhyeon7399@daum.net>
2024-01-16 16:35:05 +0900

    feat: 소셜 로그인 구현 완료 (TEST 필요)

--- zooting/src/components/signin/SocialSignInButton.vue
+++ zooting/src/components/signin/SocialSignInButton.vue
@@ -4,17 +4,21 @@
   <div class="flex flex-col">
     <div class="content-center p-4">
       <a href="http://localhost:8080/login/oauth2/authorization/kakao">
-        <img src="@/assets/images/login/kakao_login.png" />
+        <img src="@/assets/images/login/kakao_login.png" alt="Kakao Login" class="w-full h-auto" />
       </a>
     </div>
     <div class="p-4">
       <a href="http://localhost:8080/login/oauth2/authorization/google">
-        <img src="@/assets/images/login/google_login.png" />
+        <img
+          src="@/assets/images/login/google_login.png"
+          alt="Google Login"
+          class="w-full h-auto"
+        />
       </a>
     </div>
     <div class="p-4">
       <a href="http://localhost:8080/login/oauth2/authorization/naver">
-        <img src="@/assets/images/login/naver_login.png" />
+        <img src="@/assets/images/login/naver_login.png" alt="Naver Login" class="w-full h-auto" />
       </a>
     </div>
   </div>
--- zooting/src/views/SignInView.vue
+++ zooting/src/views/SignInView.vue
@@ -3,8 +3,8 @@ import SocialSignInButton from "@/components/signin/SocialSignInButton.vue";
 </script>
 
 <template>
-  <div class="flex flex-col">
-    <div class="p-8">
+  <div class="flex flex-col h-full">
+    <div class="flex items-center justify-center flex-grow p-8">
       <p class="text-center text-purple-900 text-8xl">ZOOTING</p>
     </div>
     <div class="content-center gap-2 p-4">
