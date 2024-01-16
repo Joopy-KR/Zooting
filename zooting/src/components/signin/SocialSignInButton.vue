84b1aa2fd0e06702800b8fb82fc84bb99a13972f
duckbill413 <uhyeon7399@daum.net>
2024-01-16 17:56:21 +0900

    feat: 화면 배치 업데이트 진행중

--- zooting/src/components/signin/SocialSignInButton.vue
+++ zooting/src/components/signin/SocialSignInButton.vue
@@ -2,12 +2,12 @@
 
 <template>
   <div class="flex flex-col">
-    <div class="content-center p-4">
+    <div class="content-center p-1">
       <a href="http://localhost:8080/login/oauth2/authorization/kakao">
         <img src="@/assets/images/login/kakao_login.png" alt="Kakao Login" class="w-full h-auto" />
       </a>
     </div>
-    <div class="p-4">
+    <div class="p-1">
       <a href="http://localhost:8080/login/oauth2/authorization/google">
         <img
           src="@/assets/images/login/google_login.png"
@@ -16,7 +16,7 @@
         />
       </a>
     </div>
-    <div class="p-4">
+    <div class="p-1">
       <a href="http://localhost:8080/login/oauth2/authorization/naver">
         <img src="@/assets/images/login/naver_login.png" alt="Naver Login" class="w-full h-auto" />
       </a>
--- zooting/src/views/SignInView.vue
+++ zooting/src/views/SignInView.vue
@@ -3,12 +3,21 @@ import SocialSignInButton from "@/components/signin/SocialSignInButton.vue";
 </script>
 
 <template>
-  <div class="flex flex-col h-full">
-    <div class="flex items-center justify-center flex-grow p-8">
-      <p class="text-center text-purple-900 text-8xl">ZOOTING</p>
-    </div>
-    <div class="content-center gap-2 p-4">
-      <SocialSignInButton />
+  <div class="h-screen">
+    <div class="flex flex-row items-center justify-center m-20">
+      <div class="w-1/2 pr-8 text-center">
+        <h1>Description</h1>
+      </div>
+      <div class="flex items-center justify-center w-1/2">
+        <div class="flex flex-col items-center w-1/2">
+          <div class="flex justify-center p-8">
+            <p class="text-6xl text-center text-purple-900">ZOOTING</p>
+          </div>
+          <div class="justify-center p-4">
+            <SocialSignInButton />
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
