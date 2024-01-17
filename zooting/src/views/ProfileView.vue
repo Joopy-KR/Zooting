e60d8ee57972ea5e6a21f925b45630ddac675969
duckbill413 <uhyeon7399@daum.net>
2024-01-17 17:35:08 +0900

    feat: 프로필 화면 구현 - 진행중

--- zooting/src/views/ProfileView.vue
+++ zooting/src/views/ProfileView.vue
@@ -1,18 +1,26 @@
 <script setup lang="ts"></script>
 <template>
-  <div class="flex flex-row">
-    <div class="w-1/3">
-      <div class="flex flex-col">
-        <div class="flex flex-row justify-end mr-2">
-          <p>icon</p>
-        </div>
-        <div class="flex flex-row justify-center w-1/3 border-2 rounded-full">
-          <div>sofidosnfsonfnosfn</div>
+  <div class="flex flex-row h-screen">
+    <div class="flex flex-col w-1/3">
+      <div class="flex justify-end h-1/12">
+        <p>icon</p>
+      </div>
+      <div class="flex justify-center m-4">
+        <div class="p-2 bg-gray-100 rounded-full">
+          <img src="@/assets/images/mypage/default_animal.png" class="w-full p-2" />
         </div>
       </div>
-    </div>
-    <div class="w-2/3">
-      <p>sfsfsfsfsf</p>
+      <div class="flex justify-center p-4">
+        <p class="text-3xl font-semibold">Joopy_KR</p>
+      </div>
+      <div class="flex flex-row p-2 mx-16 my-4">
+        <div class="flex items-center justify-center w-1/3 text-2xl">여성</div>
+        <div class="flex items-center justify-center w-1/3 text-2xl">20대 중반</div>
+        <div class="flex items-center justify-center w-1/3 text-2xl">서울</div>
+      </div>
+      <div class="flex justify-center m-10 text-4xl font-bold tracking-tight">
+        <p>겨울의 눈송이 개</p>
+      </div>
     </div>
   </div>
 </template>
