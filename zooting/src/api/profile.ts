991a91883f40f20f74c81dccf6336756b384e5e0
duckbill413 <uhyeon7399@daum.net>
2024-01-25 10:31:11 +0900

    feat: axios setting 사용법 주석

--- zooting/src/api/profile.ts
+++ zooting/src/api/profile.ts
@@ -7,6 +7,8 @@ async function loadMyInfoApi(success: any, fail: any) {
   await local.get(`/api/members`).then(success).catch(fail);
 }
 
+// body로는 json 데이터
+// success, fail에는 각각 성공시 반환되는 json 데이터와 실패시 생기는 error 리턴
 async function updateMyInfoApi(body: any, success: any, fail: any) {
   local.defaults.headers["Authorization"] = `Bearer ${await localStorage.getItem("accessToken")}`;
   await local.patch(`api/members/info`, body).then(success).catch(fail);
