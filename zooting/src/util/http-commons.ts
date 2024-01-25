431ee72e7f070dba37dfff8fa8353ec3d681fa9c
duckbill413 <uhyeon7399@daum.net>
2024-01-25 10:27:39 +0900

    feat: axios setting 구성

---
+++ zooting/src/util/http-commons.ts
@@ -0,0 +1,96 @@
+import axios, { HttpStatusCode } from "axios";
+import { httpStatusCode } from "./http-status";
+
+const { VITE_SERVER_API_URL } = import.meta.env;
+
+// local vue api axios instance
+function localAxios() {
+  const instance = axios.create({
+    baseURL: VITE_SERVER_API_URL,
+    // withCredentials: true,
+    // headers: {
+    //   "Content-Type": "application/json;charset=utf-8",
+    // },
+  });
+  // Request 발생 시 적용할 내용.
+  instance.defaults.headers.common["Authorization"] = "";
+  instance.defaults.headers.post["Content-Type"] = "application/json";
+  instance.defaults.headers.put["Content-Type"] = "application/json";
+  instance.defaults.headers.patch["Content-Type"] = "application/json";
+
+  // Request, Response 시 설정한 내용을 적용.
+  instance.interceptors.request.use((config) => {
+    return config;
+  }),
+    (error: any) => {
+      return Promise.reject(error);
+    };
+
+  // accessToken의 값이 유효하지 않은 경우,
+  // refreshToken을 이용해 재발급 처리.
+  // https://maruzzing.github.io/study/rnative/axios-interceptors%EB%A1%9C-%ED%86%A0%ED%81%B0-%EB%A6%AC%ED%94%84%EB%A0%88%EC%8B%9C-%ED%95%98%EA%B8%B0/
+
+  let isTokenRefreshing = false;
+  let refreshSubscribers: any = [];
+
+  const onTokenRefreshed = (accessToken: string) => {
+    refreshSubscribers.map((callback: any) => callback(accessToken));
+  };
+
+  const addRefreshSubscriber = (callback: any) => {
+    refreshSubscribers.push(callback);
+  };
+
+  instance.interceptors.response.use(
+    // 요청 성공
+    (response) => {
+      console.log(response);
+      return response;
+    },
+    // 요청 실패
+    async (error) => {
+      const {
+        config,
+        response: { status },
+      } = error;
+
+      const originalRequest = config;
+      if (status === httpStatusCode.UNAUTHORIZED) {
+        if (!isTokenRefreshing) {
+          isTokenRefreshing = true;
+
+          const refreshToken = await localStorage.getItem("refreshToken");
+          const { data } = await axios.post(VITE_SERVER_API_URL + "/api/refresh/token", {
+            refreshToken,
+          });
+          console.log(data);
+
+          const { accessToken: newAccessToken, refreshToken: newRefreshToken } = data;
+
+          await localStorage.setItem("accessToken", newAccessToken);
+          await localStorage.setItem("refreshToken", newRefreshToken);
+
+          isTokenRefreshing = false;
+
+          instance.defaults.headers.common["Authorization"] = `Bearer ${newAccessToken}`;
+          onTokenRefreshed(newAccessToken);
+        } else if (status === httpStatusCode.FORBIDDEN) {
+          alert(error.response.data.message);
+        }
+
+        const retryOriginalRequest = new Promise((resolve) => {
+          addRefreshSubscriber((accessToken: string) => {
+            originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+            resolve(instance(originalRequest));
+          });
+        });
+
+        return retryOriginalRequest;
+      }
+      return Promise.reject(error);
+    }
+  );
+  return instance;
+}
+
+export { localAxios };
