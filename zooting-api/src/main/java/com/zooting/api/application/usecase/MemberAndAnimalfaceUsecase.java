f79129c78ba8f9816c665159d8c6102c390fdbb5
wjstp <wjstp14@gmail.com>
2024-01-29 15:48:04 +0900

    feat: 동물상 변경권 로직 수정

--- zooting-api/src/main/java/com/zooting/api/application/usecase/MemberAndAnimalfaceUsecase.java
+++ zooting-api/src/main/java/com/zooting/api/application/usecase/MemberAndAnimalfaceUsecase.java
@@ -5,6 +5,7 @@ import com.zooting.api.domain.animalface.dao.AnimalFaceRepository;
 import com.zooting.api.domain.animalface.entity.AnimalFace;
 import com.zooting.api.domain.member.dao.MemberRepository;
 import com.zooting.api.domain.member.entity.Member;
+import com.zooting.api.domain.member.entity.Privilege;
 import com.zooting.api.global.common.code.ErrorCode;
 import com.zooting.api.global.exception.BaseExceptionHandler;
 import lombok.RequiredArgsConstructor;
@@ -16,11 +17,18 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberAndAnimalfaceUsecase {
     final private MemberRepository memberRepository;
     final private AnimalFaceRepository animalFaceRepository;
-
+    public static final Long DEFAULT_ANIMAL_MODIFY_PRICE = 50L;
     @Transactional
-    public void saveMemberAndAnimal(String userId, MemberAndAnimalfaceReq animalfaceReq) {
+    public boolean saveMemberAndAnimal(String userId, MemberAndAnimalfaceReq animalfaceReq) {
         Member member = memberRepository.findMemberByEmail(userId)
                 .orElseThrow(()->new BaseExceptionHandler(ErrorCode.NOT_FOUND_USER));
+        // 동물상 변경하는 유저 - 잔여 포인트 확인
+        if (member.getRole().equals(Privilege.USER)) {
+            if (member.getPoint() < DEFAULT_ANIMAL_MODIFY_PRICE) {
+                return false;
+            }
+            member.setPoint(member.getPoint()-DEFAULT_ANIMAL_MODIFY_PRICE);
+        }
         // animalFace 저장 - 동물상 닮은 비율 저장
         AnimalFace animalFace = member.getAnimalFace();
         animalFace.setAnimal1(animalfaceReq.animalfaceList().get(0));
@@ -48,7 +56,7 @@ public class MemberAndAnimalfaceUsecase {
             if (member.getGender().equals("man")) {
                 member.getAdditionalInfo().setAnimal("곰");
             } else {
-                member.getAdditionalInfo().setAnimal("꼬부기");
+                member.getAdditionalInfo().setAnimal("펭귄");
             }
         }else {
             if (member.getGender().equals("man")) {
@@ -57,13 +65,17 @@ public class MemberAndAnimalfaceUsecase {
                 member.getAdditionalInfo().setAnimal("사슴");
             }
         }
+        System.out.println(member.getRole());
+        // 멤버의 권한 수정 Anonymous 삭제하고 User 권한 부여
+        if (member.getRole().get(0).equals(Privilege.ANONYMOUS)) {
+            member.getRole().remove(Privilege.ANONYMOUS);
+            member.getRole().add(Privilege.USER);
+        }
+
         memberRepository.save(member);
         animalFace.setMember(member);
         animalFaceRepository.save(animalFace);
-
+        return true;
     }
-
-
-
 }
 
