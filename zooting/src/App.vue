c9c64a90c718c83c677e7ca40380ba21f4f7e921
김재형 <kjhstar971124@gmail.com>
2024-01-30 18:00:59 +0900

    feat: openvidu 진행중

--- zooting/src/App.vue
+++ zooting/src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="flex">
     <TheSideBar/>
-    <RouterView class="ms-14"/>
+    <!-- <RouterView class="ms-14"/> -->
+    <test />
   </div>
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import TheSideBar from '@/components/TheSideBar.vue'
+import test from '@/components/video-chat/test.vue'
 </script>
 
 <style scoped>
