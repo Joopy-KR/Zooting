86ff6a46c125e2e6228553107c3f4caf3cd404fa
jeghtner <jeghtner@gmail.com>
2024-02-02 15:57:35 +0900

    fix/#70 : DMDTo 수정, DMFileRes 구현

--- zooting-api/src/main/java/com/zooting/api/domain/dm/api/DMWebSocketController.java
+++ zooting-api/src/main/java/com/zooting/api/domain/dm/api/DMWebSocketController.java
@@ -9,11 +9,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@PreAuthorize("hasRole('USER')")
+//@PreAuthorize("hasRole('USER')")
 @RequiredArgsConstructor
 @RestController
 @Tag(name = "DM 웹소켓", description = "DM웹소켓 관련 API")
