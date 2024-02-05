dfb4db6470929c5d896038a257cc98ce321d795a
wjstp <wjstp14@gmail.com>
2024-02-05 14:26:11 +0900

    feat: 공지사항 조회 응답시시 DTO 수정

--- zooting-api/src/main/java/com/zooting/api/domain/notice/dao/NoticeRepository.java
+++ zooting-api/src/main/java/com/zooting/api/domain/notice/dao/NoticeRepository.java
@@ -1,14 +1,14 @@
 package com.zooting.api.domain.notice.dao;
 
 import com.zooting.api.domain.notice.entity.Notice;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
     Optional<Notice> findNoticeById(Long noticeId);
-    Slice<Notice> findNoticesBy(Pageable pageable);
+    Page<Notice> findNoticesBy(Pageable pageable);
     void deleteNoticeById(Long noticeId);
 }
