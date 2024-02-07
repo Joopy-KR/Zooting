c382b08afd4f93035f157752017da536bad37c88
duckbill413 <uhyeon7399@daum.net>
2024-02-07 15:26:35 +0900

    feat: 유저 회원가입 초기 포인트

--- zooting-api/src/main/java/com/zooting/api/global/security/userdetails/service/CustomUserDetailsService.java
+++ zooting-api/src/main/java/com/zooting/api/global/security/userdetails/service/CustomUserDetailsService.java
@@ -5,9 +5,11 @@ import com.zooting.api.domain.member.entity.Member;
 import com.zooting.api.domain.member.entity.Privilege;
 import com.zooting.api.global.security.userdetails.CustomUserDetails;
 import jakarta.transaction.Transactional;
+
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
@@ -21,6 +23,7 @@ import org.springframework.stereotype.Component;
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
+
     public CustomUserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         Optional<Member> optionalMember = memberRepository.findMemberByEmail(email);
         Member member = optionalMember.orElseGet(() -> registerMember(email));
@@ -32,14 +35,15 @@ public class CustomUserDetailsService implements UserDetailsService {
                 .build();
     }
 
-    private Member registerMember(String email){
+    private Member registerMember(String email) {
         return memberRepository.save(Member.builder()
                 .email(email)
+                .point(200L) // 시작시 초기 자금
                 .role(List.of(Privilege.ANONYMOUS))
                 .build());
     }
 
-    protected Collection<? extends GrantedAuthority> mapPrivilegeToAuthorities(Member member){
+    protected Collection<? extends GrantedAuthority> mapPrivilegeToAuthorities(Member member) {
         return AuthorityUtils.createAuthorityList(member.getRole().stream().map(Enum::name).toList());
     }
 }
