79fc4b16af67f4793ca47d20fc76f45343d839a9
2yunj007 <2yunj007@gmail.com>
2024-02-10 17:32:21 +0900

    feat: 검색 결과 css

--- zooting/src/components/home/Social.vue
+++ zooting/src/components/home/Social.vue
@@ -14,7 +14,7 @@
 <script setup lang="ts">
 import { ref, shallowRef, onMounted, computed } from 'vue'
 import { useAccessTokenStore } from '@/stores/store'
-import Search from '@/components/Search.vue'
+import Search from '@/components/home/Search.vue'
 import SocialTabBar from '@/components/home/SocialTabBar.vue'
 import SocialFriendList from '@/components/home/SocialFriendList.vue'
 import SocialRequestList from '@/components/home/SocialRequestList.vue'
