22ac9242306bed658672d1f3b83630e6c7858d9b
2yunj007 <2yunj007@gmail.com>
2024-02-11 17:13:47 +0900

    feat: 매칭 시작 버튼 css

--- zooting/src/components/home/ReadyMatch.vue
+++ zooting/src/components/home/ReadyMatch.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="match__container">
     <div class="matching-button">
-      <img :src="matchingButton()" alt="matching-button" @click="meetingRegister">
+      <img :src="matchingButton()" alt="matching-button">
+      <button v-if="!store.isMatching" class="btn-hover color-1" @click="meetingRegister">Match</button>
+      <button v-if="store.isMatching" class="btn-hover color-2" @click="meetingExit">Cancel</button>
     </div>
     <div class="matching" v-show="store.isMatching">
       <div class="matching__text">
-				Matching...
+        Matching...
       </div>
       <div class="matching__timer">
-				{{ store.formattedTimer }}      
-			</div>
-			<button class="btn-hover color-11" @click="meetingExit">Cancel</button>
+        {{ store.formattedTimer }}      
+      </div>
     </div>
   </div>
 </template>
@@ -47,7 +48,7 @@ const meetingExit = () => {
   @apply flex flex-col justify-center items-center relative;
 }
 .matching-button {
-  @apply flex justify-center items-center relative cursor-pointer;
+  @apply flex justify-center items-center relative;
 }
 .matching-button img {
   @apply w-5/6;
@@ -62,33 +63,38 @@ const meetingExit = () => {
   @apply text-3xl;
 }
 .btn-hover {
-	@apply absolute top-60;
-    width: 150px;
-    font-size: 20px;
-    /* font-weight: 600; */
-    color: #fff;
-    cursor: pointer;
-    margin: 20px;
-    height: 50px;
-    text-align:center;
-    border: none;
-    background-size: 300% 100%;
+	@apply absolute -bottom-24;
+  width: 170px;
+  font-size: 25px;
+  /* font-weight: 600; */
+  color: #fff;
+  cursor: pointer;
+  margin: 20px;
+  height: 55px;
+  text-align:center;
+  border: none;
+  background-size: 300% 100%;
 
-    border-radius: 50px;
-    -o-transition: all .4s ease-in-out;
-    -webkit-transition: all .4s ease-in-out;
-    transition: all .4s ease-in-out;
+  border-radius: 50px;
+  -o-transition: all .4s ease-in-out;
+  -webkit-transition: all .4s ease-in-out;
+  transition: all .4s ease-in-out;
 }
 .btn-hover:hover {
-    background-position: 100% 0;
-    -o-transition: all .4s ease-in-out;
-    -webkit-transition: all .4s ease-in-out;
-    transition: all .4s ease-in-out;
+  background-position: 100% 0;
+  -o-transition: all .4s ease-in-out;
+  -webkit-transition: all .4s ease-in-out;
+  transition: all .4s ease-in-out;
 }
 .btn-hover:focus {
-    outline: none;
+  outline: none;
 }
-.btn-hover.color-11 {
-  background-image: linear-gradient(to right, #eb3941, #f15e64, #e14e53, #e2373f);  box-shadow: 0 5px 15px rgba(242, 97, 103, .4);
+.btn-hover.color-1 {
+  background-image: linear-gradient(to right, #25aae1, #4481eb, #04befe, #3f86ed);
+  box-shadow: 0 4px 15px 0 rgba(65, 132, 234, 0.75);
+}
+.btn-hover.color-2 {
+  background-image: linear-gradient(to right, #eb3941, #f15e64, #e14e53, #e2373f);  
+  box-shadow: 0 5px 15px rgba(242, 97, 103, .4);
 }
 </style>
